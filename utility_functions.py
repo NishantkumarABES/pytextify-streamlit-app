1990987ea7d271d503a1d8d5262e5006543fd021
Nishant Kumar <nishant543099@gmail.com>
2024-12-07 23:51:50 +0530

    youtube transcription featcher

--- utility_functions.py
+++ utility_functions.py
@@ -1,4 +1,10 @@
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+
 
 def is_valid_email(email):
     if '@' not in email or email.startswith('@') or email.endswith('@'):
@@ -12,18 +18,38 @@ def is_valid_email(email):
         return False
     return True
 
+CHROMEDRIVER_PATH = 'assets/selenium/chromedriver.exe'
+
+
+def initialize_driver(headless = True):
+    chrome_options = Options()
+    if headless: chrome_options.add_argument('--headless')  
+    service = Service(CHROMEDRIVER_PATH)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    return driver
 
 def fetch_transcript(video_url):
-    url = "http://127.0.0.1:5000/fetch_transcript"  # Replace with your Flask app's URL if different
-    data = {'video_url': video_url}
+    title_xpath = '//*[@id="title"]/h1/yt-formatted-string'
+    more_xpath = '//*[@id="expand"]'
+    show_transcript_xpath = '//*[@id="primary-button"]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]'
+    driver = initialize_driver() 
+    driver.get(video_url)
+    time.sleep(5)
+    video_title = driver.find_element(By.XPATH, title_xpath).text
+    time.sleep(2)
+    driver.find_element(By.XPATH, more_xpath).click()
+    time.sleep(2)
+    driver.find_element(By.XPATH, show_transcript_xpath).click()
+    time.sleep(2)
+    transcript, index = '', 1
+    while True:
+        transcript_xpath = f'//*[@id="segments-container"]/ytd-transcript-segment-renderer[{index}]/div/yt-formatted-string'
+        try:
+            sentence = driver.find_element(By.XPATH, transcript_xpath).text
+        except:
+            break
+        transcript += sentence + '\n'
+        index += 1
     
-    try:
-        response = requests.post(url, data=data)
-        response.raise_for_status()  # Raises an error for HTTP codes 4xx/5xx
-        result = response.json()  # Parse the response JSON
-        return result
-        # print("Video Title:", result['video_title'])
-        # print("\nTranscription:\n", result['transcription'])
+    return {'video_title': video_title,'transcription': transcript}
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching transcript: {e}")
